32f22ec982dae44d55c970ecd938f3f8ade12f8f
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-21 16:14:29 +1100

    Compiles nice

--- src/Device.Net.UWP/UWPDeviceBase.cs
+++ src/Device.Net.UWP/UWPDeviceBase.cs
@@ -80,6 +80,29 @@ namespace Device.Net.UWP
         {
             return _ConnectedDevice != null;
         }
+
+        public override async Task<byte[]> ReadAsync()
+        {
+            if (_IsReading)
+            {
+                throw new Exception("Reentry");
+            }
+
+            lock (_Chunks)
+            {
+                if (_Chunks.Count > 0)
+                {
+                    var retVal = _Chunks[0];
+                    Tracer?.Trace(false, retVal);
+                    _Chunks.RemoveAt(0);
+                    return retVal;
+                }
+            }
+
+            _IsReading = true;
+            _TaskCompletionSource = new TaskCompletionSource<byte[]>();
+            return await _TaskCompletionSource.Task;
+        }
         #endregion
 
         #region Public Virtual Methods
--- src/Hid.Net.UWP/UWPHidDevice.cs
+++ src/Hid.Net.UWP/UWPHidDevice.cs
@@ -73,28 +73,7 @@ namespace Hid.Net.UWP
 
         #region Public Methods
 
-        public override async Task<byte[]> ReadAsync()
-        {
-            if (_IsReading)
-            {
-                throw new Exception("Reentry");
-            }
 
-            lock (_Chunks)
-            {
-                if (_Chunks.Count > 0)
-                {
-                    var retVal = _Chunks[0];
-                    Tracer?.Trace(false, retVal);
-                    _Chunks.RemoveAt(0);
-                    return retVal;
-                }
-            }
-
-            _IsReading = true;
-            _TaskCompletionSource = new TaskCompletionSource<byte[]>();
-            return await _TaskCompletionSource.Task;
-        }
 
         public override async Task WriteAsync(byte[] data)
         {
