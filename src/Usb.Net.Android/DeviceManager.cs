531c7d15959e864059f79489581bf77eb0ea119a
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-22 13:14:41 +1100

    Comment out some code

--- src/Usb.Net.Android/DeviceManager.cs
+++ src/Usb.Net.Android/DeviceManager.cs
@@ -1,39 +1,39 @@
-﻿using Android.Hardware.Usb;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿//using Android.Hardware.Usb;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
 
-namespace Device.Net
-{
-    public class DeviceManager
-    {
-        public UsbManager UsbManager { get; }
+//namespace Device.Net
+//{
+//    public class DeviceManager
+//    {
+//        public UsbManager UsbManager { get; }
 
-        #region Public Static Properties
-        public static DeviceManager Current { get; set; }
-        #endregion
+//        #region Public Static Properties
+//        public static DeviceManager Current { get; set; }
+//        #endregion
 
-        #region Constructor
-        public DeviceManager(UsbManager usbManager)
-        {
-            UsbManager = usbManager;
-        }
-        #endregion
+//        #region Constructor
+//        public DeviceManager(UsbManager usbManager)
+//        {
+//            UsbManager = usbManager;
+//        }
+//        #endregion
 
-        #region Public Methods
-        public async Task<IEnumerable<DeviceDefinition>> GetConnectedDeviceDefinitions(uint? vendorId, uint? productId, DeviceType deviceType)
-        {
-            if (deviceType == DeviceType.Hid) throw new Exception("Android does not support Hid");
+//        #region Public Methods
+//        public async Task<IEnumerable<DeviceDefinition>> GetConnectedDeviceDefinitions(uint? vendorId, uint? productId, DeviceType deviceType)
+//        {
+//            if (deviceType == DeviceType.Hid) throw new Exception("Android does not support Hid");
 
-            var devices = UsbManager.DeviceList.Select(kvp => kvp.Value).ToList();
+//            var devices = UsbManager.DeviceList.Select(kvp => kvp.Value).ToList();
 
-            //TODO: return the vid/pid if we can get it from the properties. Also read/write buffer size
+//            //TODO: return the vid/pid if we can get it from the properties. Also read/write buffer size
 
-            var deviceIds = devices.Select(d => new DeviceDefinition { DeviceId = d.DeviceId.ToString(), ProductId = (uint)d.ProductId, VendorId = (uint)d.VendorId, DeviceType = deviceType }).ToList();
+//            var deviceIds = devices.Select(d => new DeviceDefinition { DeviceId = d.DeviceId.ToString(), ProductId = (uint)d.ProductId, VendorId = (uint)d.VendorId, DeviceType = deviceType }).ToList();
 
-            return deviceIds;
-        }
-        #endregion
-    }
-}
+//            return deviceIds;
+//        }
+//        #endregion
+//    }
+//}
