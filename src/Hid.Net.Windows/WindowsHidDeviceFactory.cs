a9ac2c0f5c58178250518ceb2d7ec679aaefe7a6
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-22 12:39:51 +1100

    Hid works!

--- src/Hid.Net.Windows/WindowsHidDeviceFactory.cs
+++ src/Hid.Net.Windows/WindowsHidDeviceFactory.cs
@@ -66,15 +66,89 @@ namespace Hid.Net.Windows
 
                     APICalls.SetupDiGetDeviceInterfaceDetail(i, ref spDeviceInterfaceData, ref spDeviceInterfaceDetailData, 256, out _, ref spDeviceInfoData);
 
-                    var deviceInformation = new DeviceDefinition { DeviceId = spDeviceInterfaceDetailData.DevicePath };
+                    //Note this is a bit nast but we can filter Vid and Pid this way I think...
+                    var vendorHex = vendorId?.ToString("X").ToLower().PadLeft(4, '0');
+                    var productIdHex = productId?.ToString("X").ToLower().PadLeft(4, '0');
+                    if (vendorId.HasValue && !spDeviceInterfaceDetailData.DevicePath.ToLower().Contains(vendorHex)) continue;
+                    if (productId.HasValue && !spDeviceInterfaceDetailData.DevicePath.ToLower().Contains(productIdHex)) continue;
 
-                    deviceInformations.Add(deviceInformation);
+                    deviceInformations.Add(GetDeviceInformation(spDeviceInterfaceDetailData.DevicePath));
                 }
 
                 APICalls.SetupDiDestroyDeviceInfoList(i);
 
                 return deviceInformations;
             });
+        }
+
+        private static WindowsHidDeviceInformation GetDeviceInformation(string devicePath)
+        {
+            using (var safeFileHandle = APICalls.CreateFile(devicePath, APICalls.GenericRead | APICalls.GenericWrite, APICalls.FileShareRead | APICalls.FileShareWrite, IntPtr.Zero, APICalls.OpenExisting, 0, IntPtr.Zero))
+            {
+                var hidCollectionCapabilities = new HidCollectionCapabilities();
+                var hidAttributes = new HidAttributes();
+                var pointerToPreParsedData = new IntPtr();
+                var product = string.Empty;
+                var serialNumber = string.Empty;
+                var manufacturer = string.Empty;
+                var pointerToBuffer = Marshal.AllocHGlobal(126);
+
+                var preparsedDataResult = HidAPICalls.HidD_GetPreparsedData(safeFileHandle, ref pointerToPreParsedData);
+                if (!preparsedDataResult)
+                {
+                    return null;
+                }
+
+                //TODO: Deal with issues here
+
+                var getCapsResult = HidAPICalls.HidP_GetCaps(pointerToPreParsedData, ref hidCollectionCapabilities);
+
+                //TODO: Deal with issues here
+
+                if (!HidAPICalls.HidD_GetAttributes(safeFileHandle, ref hidAttributes))
+                {
+                    throw new Exception("Could not obtain attributes");
+                }
+
+                if (HidAPICalls.HidD_GetManufacturerString(safeFileHandle, pointerToBuffer, 126))
+                {
+                    manufacturer = Marshal.PtrToStringUni(pointerToBuffer);
+                }
+
+                if (HidAPICalls.HidD_GetSerialNumberString(safeFileHandle, pointerToBuffer, 126))
+                {
+                    serialNumber = Marshal.PtrToStringUni(pointerToBuffer);
+                }
+
+                if (HidAPICalls.HidD_GetProductString(safeFileHandle, pointerToBuffer, 126))
+                {
+                    product = Marshal.PtrToStringUni(pointerToBuffer);
+                }
+
+                Marshal.FreeHGlobal(pointerToBuffer);
+
+                var getPreparsedDataResult = HidAPICalls.HidD_FreePreparsedData(ref pointerToPreParsedData);
+
+                //TODO: Deal with issues here
+
+                var deviceInformation = new WindowsHidDeviceInformation
+                {
+                    DeviceId = devicePath,
+                    //TODO Is this the right way around?
+                    WriteBufferSize = hidCollectionCapabilities.InputReportByteLength,
+                    ReadBufferSize = hidCollectionCapabilities.OutputReportByteLength,
+                    Manufacturer = manufacturer,
+                    Product = product,
+                    ProductId = (ushort)hidAttributes.ProductId,
+                    SerialNumber = serialNumber,
+                    Usage = hidCollectionCapabilities.Usage,
+                    UsagePage = hidCollectionCapabilities.UsagePage,
+                    VendorId = (ushort)hidAttributes.VendorId,
+                    VersionNumber = (ushort)hidAttributes.VersionNumber
+                };
+
+                return deviceInformation;
+            }
         }
     }
 }
