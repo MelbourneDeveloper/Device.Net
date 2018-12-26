77a2e2dbf2836f49d4508be3badb58be5a15c206
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-27 08:25:38 +1100

    A little bit of work

--- src/Usb.Net.WindowsSample/Program.cs
+++ src/Usb.Net.WindowsSample/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace Usb.Net.WindowsSample
 {
@@ -6,7 +7,45 @@ namespace Usb.Net.WindowsSample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Go();
+            Console.ReadLine();
+        }
+
+        private async static Task Go()
+        {
+            //Register the factory for creating Usb devices. This only needs to be done once.
+            UWPUsbDeviceFactory.Register();
+
+            //Register the factory for creating Usb devices. This only needs to be done once.
+            UWPHidDeviceFactory.Register();
+
+            //Note: other custom device types could be added here
+
+            //Define the types of devices to search for. This particular device can be connected to via USB, or Hid
+            var deviceDefinitions = new List<DeviceDefinition>
+            {
+                new DeviceDefinition{ DeviceType= DeviceType.Hid, VendorId= 0x534C, ProductId=0x0001, Label="Trezor One Firmware 1.6.x" },
+                new DeviceDefinition{ DeviceType= DeviceType.Usb, VendorId= 0x1209, ProductId=0x53C1, ReadBufferSize=64, WriteBufferSize=64, Label="Trezor One Firmware 1.7.x" },
+                new DeviceDefinition{ DeviceType= DeviceType.Usb, VendorId= 0x1209, ProductId=0x53C0, ReadBufferSize=64, WriteBufferSize=64, Label="Model T" }
+            };
+
+            //Get the first available device and connect to it
+            var devices = await DeviceManager.Current.GetDevices(deviceDefinitions);
+            var trezorDevice = devices.FirstOrDefault();
+            await trezorDevice.InitializeAsync();
+
+            //Create a buffer with 3 bytes (initialize)
+            var buffer = new byte[64];
+            buffer[0] = 0x3f;
+            buffer[1] = 0x23;
+            buffer[2] = 0x23;
+
+            //Write the data to the device
+            await trezorDevice.WriteAsync(buffer);
+
+            //Read the response
+            var readBuffer = await trezorDevice.ReadAsync();
+
         }
     }
 }
