e7a19d7c680f68b08525d57793a58aecfb128d7c
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-29 15:07:26 +1100

    Get it compiling

--- src/Usb.Net.Android/DeviceManager.cs
+++ src/Usb.Net.Android/DeviceManager.cs
@@ -10,12 +10,12 @@ namespace Device.Net
 {
     public class AndroidDeviceFactoryBase : IDeviceFactory
     {
+        #region Public Properties
         public UsbManager UsbManager { get; }
         public Context Context { get; }
+        #endregion
 
         #region Public Static Properties
-        public static DeviceManager Current { get; set; }
-
         public DeviceType DeviceType => throw new NotImplementedException();
         #endregion
 
@@ -28,15 +28,23 @@ namespace Device.Net
         #endregion
 
         #region Public Methods
-        public async Task<IEnumerable<DeviceDefinition>> GetConnectedDeviceDefinitions(uint? vendorId, uint? productId)
+        public Task<IEnumerable<DeviceDefinition>> GetConnectedDeviceDefinitions(uint? vendorId, uint? productId)
         {
             //TODO: Get the values necessary to construct the device.
-            return UsbManager.DeviceList.Select(kvp => kvp.Value).Select(d => new DeviceDefinition { DeviceId = d.DeviceId.ToString(), ProductId = (uint)d.ProductId, VendorId = (uint)d.VendorId, DeviceType = DeviceType.Usb }).ToList();
+            var deviceDefinitions = UsbManager.DeviceList.Select(kvp => kvp.Value).Select(d => new DeviceDefinition { DeviceId = d.DeviceId.ToString(), ProductId = (uint)d.ProductId, VendorId = (uint)d.VendorId, DeviceType = DeviceType.Usb }).ToList();
+            return Task.FromResult<IEnumerable<DeviceDefinition>>(deviceDefinitions);
         }
 
         public IDevice GetDevice(DeviceDefinition deviceDefinition)
         {
             return new AndroidUsbDevice(UsbManager, Context, 3000, deviceDefinition.ReadBufferSize.Value, (int)deviceDefinition.VendorId.Value, (int)deviceDefinition.ProductId.Value);
+        }
+        #endregion
+
+        #region Public Static Methods
+        public static void Register(UsbManager usbManager, Context context)
+        {
+            DeviceManager.Current.DeviceFactories.Add(new AndroidDeviceFactoryBase(usbManager, context));
         }
         #endregion
     }
