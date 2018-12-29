3e507dbbf57a23e11795713d8d775f17883153b5
MelbourneDeveloper <christian_findlay@hotmail.com>
2018-12-29 15:12:43 +1100

    Get more device information

--- src/Device.Net/Windows/WindowsDeviceDefinition.cs
+++ src/Device.Net/Windows/WindowsDeviceDefinition.cs
@@ -4,9 +4,6 @@ namespace Device.Net.Windows
 {
     public class WindowsDeviceDefinition : DeviceDefinition
     {
-        public string Manufacturer { get; set; }
-        public string Product { get; set; }
-        public string SerialNumber { get; set; }
         public ushort Usage { get; set; }
         public ushort UsagePage { get; set; }
         public ushort VersionNumber { get; set; }
