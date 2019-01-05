dd7a554b8b086c6ab67e93021642f6385da18f56
MelbourneDeveloper <christian_findlay@hotmail.com>
2019-01-05 15:43:46 +1100

    A comment

--- src/Device.Net/DevicePoller.cs
+++ src/Device.Net/DevicePoller.cs
@@ -85,6 +85,7 @@ namespace Device.Net
                         if (device.IsInitialized)
                         {
                             //Let listeners know a registered device was disconnected
+                            //NOTE: let the rest of the app know before disposal so that the app can stop doing whatever it's doing.
                             DeviceDisconnected?.Invoke(this, new DeviceEventArgs(device));
 
                             //The device is no longer connected so disconnect it
