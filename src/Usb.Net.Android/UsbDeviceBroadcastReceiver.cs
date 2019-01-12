2f9a709d5354485e91508d40829e9ed92ebc3d5d
MelbourneDeveloper <christian_findlay@hotmail.com>
2019-01-12 12:43:59 +1100

    Only have one type of receiver

---
+++ src/Usb.Net.Android/UsbDeviceBroadcastReceiver.cs
@@ -0,0 +1,33 @@
+﻿using Android.Content;
+using Android.Hardware.Usb;
+using Device.Net;
+using System;
+
+namespace Usb.Net.Android
+{
+    public class UsbDeviceBroadcastReceiver : BroadcastReceiver
+    {
+        #region Fields
+        private readonly DeviceListener _DeviceListener;
+        #endregion
+
+        #region Constructor
+        public UsbDeviceBroadcastReceiver(DeviceListener deviceListener)
+        {
+            _DeviceListener = deviceListener;
+        }
+        #endregion
+
+        #region Overrides
+        public override async void OnReceive(Context context, Intent intent)
+        {
+            //No need to get the device because we're going to enumerate them anyway...
+            //var device = intent.GetParcelableExtra(UsbManager.ExtraDevice) as UsbDevice;
+
+            await _DeviceListener.CheckForDevicesAsync();
+
+            Logger.Log("Device connected", null, AndroidUsbDevice.LogSection);
+        }
+        #endregion
+    }
+}
