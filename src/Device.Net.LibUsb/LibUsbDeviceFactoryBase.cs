175bf0dfab09d0b951949524f78ad8f2f3493b45
MelbourneDeveloper <christian_findlay@hotmail.com>
2019-02-03 11:04:40 +1100

    Lay out the skeleton of GetConnectedDeviceDefinitionsAsync

--- src/Device.Net.LibUsb/LibUsbDeviceFactoryBase.cs
+++ src/Device.Net.LibUsb/LibUsbDeviceFactoryBase.cs
@@ -1,17 +1,39 @@
-﻿using System;
+﻿using LibUsbDotNet;
+using LibUsbDotNet.Main;
+using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace Device.Net.LibUsb
 {
     public abstract class LibUsbDeviceFactoryBase : IDeviceFactory
     {
-        public abstract DeviceType DeviceType {get;}
+        public abstract DeviceType DeviceType { get; }
 
-        public Task<IEnumerable<ConnectedDeviceDefinition>> GetConnectedDeviceDefinitionsAsync(FilterDeviceDefinition deviceDefinition)
+        public async Task<IEnumerable<ConnectedDeviceDefinition>> GetConnectedDeviceDefinitionsAsync(FilterDeviceDefinition deviceDefinition)
         {
-            throw new NotImplementedException();
+            return await Task.Run(() =>
+            {
+                UsbDeviceFinder usbDeviceFinder = null;
+
+                if (deviceDefinition.VendorId.HasValue && !deviceDefinition.ProductId.HasValue)
+                {
+                    usbDeviceFinder = new UsbDeviceFinder((int)deviceDefinition.VendorId.Value);
+                }
+
+                var usbDevice = UsbDevice.OpenUsbDevice(usbDeviceFinder);
+
+                var retVal = new List<ConnectedDeviceDefinition>();
+
+                if (usbDevice != null)
+                {
+                    retVal.Add(new ConnectedDeviceDefinition(usbDevice.UsbRegistryInfo.DeviceInterfaceGuids[0].ToString()));
+                    usbDevice.Close();
+                    return retVal;
+                }
+
+                return retVal;
+            });
         }
 
         public IDevice GetDevice(ConnectedDeviceDefinition deviceDefinition)
