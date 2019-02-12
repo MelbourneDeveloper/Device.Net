8bdb2e34347ac98806104441ebcc62119b54140b
MelbourneDeveloper <christian_findlay@hotmail.com>
2019-02-12 17:21:11 +1100

    Start implementing code rules

---
+++ src/Device.Net/GlobalSuppressions.cs
@@ -0,0 +1,15 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "<Pending>", Scope = "member", Target = "~F:Device.Net.Windows.SpDeviceInterfaceDetailData.DevicePath")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "<Pending>", Scope = "member", Target = "~F:Device.Net.Windows.SpDeviceInfoData.Reserved")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "<Pending>", Scope = "member", Target = "~F:Device.Net.Windows.SpDeviceInfoData.CbSize")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "<Pending>", Scope = "member", Target = "~F:Device.Net.Windows.SpDeviceInfoData.ClassGuid")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "<Pending>", Scope = "member", Target = "~F:Device.Net.Windows.SpDeviceInfoData.DevInst")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "<Pending>", Scope = "member", Target = "~F:Device.Net.Windows.SpDeviceInterfaceData.CbSize")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "<Pending>", Scope = "member", Target = "~F:Device.Net.Windows.SpDeviceInterfaceData.Flags")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "<Pending>", Scope = "member", Target = "~F:Device.Net.Windows.SpDeviceInterfaceData.InterfaceClassGuid")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1051:Do not declare visible instance fields", Justification = "<Pending>", Scope = "member", Target = "~F:Device.Net.Windows.SpDeviceInterfaceData.Reserved")]
