56d24abc4ea772822da23ce6f6ea348ec6d246e0
MelbourneDeveloper <christian_findlay@hotmail.com>
2019-02-14 19:40:57 +1100

    More rules stuff

--- src/Device.Net/Helpers.cs
+++ src/Device.Net/Helpers.cs
@@ -3,7 +3,7 @@
     /// <summary> 
     /// Provides helpers for all platforms. 
     /// </summary> 
-    public class Helpers
+    public static class Helpers
     {
         public const string WriteErrorMessage = "An error occurred while attempting to write to the device";
         public const string ReadErrorMessage = "An error occurred while attempting to read from the device";
