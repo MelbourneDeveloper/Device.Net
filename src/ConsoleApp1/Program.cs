3a86ccbb3def432d838ca4af8775a7c160a055a1
MelbourneDeveloper <christian_findlay@hotmail.com>
2019-02-14 22:06:40 +1100

    Smash more rules in

---
+++ src/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var asdasd = "Hello".ContainsIgnoreCase("hella");
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public static class ass
+    {
+        public static bool ContainsIgnoreCase(this string paragraph, string word)
+        {
+            return new CultureInfo("en-US").CompareInfo.IndexOf(paragraph, word, CompareOptions.IgnoreCase) >= 0;
+        }
+
+    }
+}
