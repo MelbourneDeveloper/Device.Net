e2b95d41eef82d582244b881c57c5be5adb6ada1
MelbourneDeveloper <christian_findlay@hotmail.com>
2019-03-31 10:38:47 +1100

    More refactoring

---
+++ src/Device.Net.UnitTests/MockDeviceBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+
+namespace Device.Net.UnitTests
+{
+    public abstract class MockDeviceBase : DeviceBase, IDevice
+    {
+        public override ushort WriteBufferSize => 64;
+        public override ushort ReadBufferSize => 64;
+
+        protected bool _IsInitialized;
+
+        public override bool IsInitialized => _IsInitialized;
+
+        public void Close()
+        {
+        }
+
+        public Task InitializeAsync()
+        {
+            _IsInitialized = true;
+            return Task.FromResult(true);
+        }
+
+        public override Task<byte[]> ReadAsync()
+        {
+            return Task.FromResult(new byte[64] { 1, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
+        }
+
+        public override Task WriteAsync(byte[] data)
+        {
+            return Task.FromResult(true);
+        }
+    }
+}
--- src/Device.Net.UnitTests/MockHidDevice.cs
+++ src/Device.Net.UnitTests/MockHidDevice.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Threading.Tasks;
-
-namespace Device.Net.UnitTests
+﻿namespace Device.Net.UnitTests
 {
-    public class MockHidDevice : DeviceBase, IDevice
+    public class MockHidDevice : MockDeviceBase, IDevice
     {
         public const uint ProductId = 1;
         public const uint VendorId = 1;
@@ -15,33 +12,5 @@ namespace Device.Net.UnitTests
             ConnectedDeviceDefinition = new ConnectedDeviceDefinition(DeviceId) { ProductId = ProductId, VendorId = VendorId };
         }
 
-        private bool _IsInitialized;
-
-        public override ushort WriteBufferSize => 64;
-
-        public override ushort ReadBufferSize => 64;
-
-        public override bool IsInitialized => _IsInitialized;
-
-        public void Close()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task InitializeAsync()
-        {
-            _IsInitialized = true;
-            return Task.FromResult(true);
-        }
-
-        public override Task<byte[]> ReadAsync()
-        {
-            return Task.FromResult(new byte[64] { 1, 2, 3, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 });
-        }
-
-        public override Task WriteAsync(byte[] data)
-        {
-            return Task.FromResult(true);
-        }
     }
 }
