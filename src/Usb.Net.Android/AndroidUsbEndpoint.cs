971daaace75972fc29e610e01be2bd2189044d45
Christian <anemail@nothing.com>
2019-07-06 10:03:59 +1000

    Start breaking up the interface model

---
+++ src/Usb.Net.Android/AndroidUsbEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using Android.Hardware.Usb;
+
+namespace Usb.Net.Android
+{
+    public class AndroidUsbEndpoint : IUsbInterfaceEndpoint
+    {
+        private readonly UsbEndpoint UsbEndpoint;
+        public bool IsRead { get; }
+        public bool IsWrite { get; }
+        public byte PipeId { get; }
+
+        public AndroidUsbEndpoint(UsbEndpoint usbEndpoint, bool isRead, bool isWrite, byte pipeId)
+        {
+            IsRead = isRead;
+            IsWrite = isWrite;
+            UsbEndpoint = usbEndpoint;
+            PipeId = pipeId;
+        }
+    }
+}
