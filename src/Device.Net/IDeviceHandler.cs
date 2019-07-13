bf45bb9889c9a1af948a37e40d9af3ce133bc91c
Christian <anemail@nothing.com>
2019-07-13 11:14:40 +1000

    Break an interface out

---
+++ src/Device.Net/IDeviceHandler.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Device.Net
+{
+    public interface IDeviceHandler : IDisposable
+    {
+        ushort WriteBufferSize { get; }
+        ushort ReadBufferSize { get; }
+        bool IsInitialized { get; }
+        Task InitializeAsync();
+        void Close();
+    }
+}
