75c2f76c5af6dd70699c9f5188322ab019cae086
Ivan Kara <codexana133@gmail.com>
2019-08-13 12:10:24 +0700

    Added test cli app

---
+++ src/testusb/Program.cs
@@ -0,0 +1,21 @@
+﻿using Device.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Usb.Net.Windows;
+
+namespace testusb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            WindowsUsbDeviceFactory.Register(null, null);
+            var x = DeviceManager.Current.GetConnectedDeviceDefinitionsAsync(null).Result;
+            ;
+        }
+    }
+}
