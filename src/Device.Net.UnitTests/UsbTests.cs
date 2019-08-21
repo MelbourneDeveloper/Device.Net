7fb2c40d0fcb52e4ae9510d46d5ef20b57af158b
Christian Findlay <a@a.com>
2019-08-21 18:24:30 +1000

    Testing up dem units

--- src/Device.Net.UnitTests/UsbTests.cs
+++ src/Device.Net.UnitTests/UsbTests.cs
@@ -8,21 +8,61 @@ namespace Device.Net.UnitTests
     [TestClass]
     public class UsbTests
     {
+        #region Fields
+        private UsbDevice _UsbDevice;
+        private const string deviceId = "test";
+        private readonly byte[] testreadpacket = { 1, 2, 3 };
+        #endregion
+
+        #region Tests
         [TestMethod]
         public async Task TestUsbDeviceInitialization()
         {
-            const string deviceId = "test";
+            await InitializeDevice();
+
+            Assert.IsNotNull(_UsbDevice.ConnectedDeviceDefinition);
+            Assert.AreEqual(deviceId, ((ConnectedDeviceDefinition)_UsbDevice.ConnectedDeviceDefinition).DeviceId);
+        }
+
+        [TestMethod]
+        public async Task TestRead()
+        {
+            await InitializeDevice();
+            var readResult = await _UsbDevice.ReadAsync();
+            Assert.AreEqual(readResult.Data, testreadpacket);
+        }
+
+        [TestMethod]
+        public async Task TestWrite()
+        {
+            await InitializeDevice();
+            await _UsbDevice.WriteAsync(testreadpacket);
+            await _UsbDevice.UsbInterfaceManager.WriteUsbInterface.Received().WriteAsync(testreadpacket);
+        }
+        #endregion
+
+        #region Helpers
+        private async Task InitializeDevice()
+        {
+            if (_UsbDevice != null) return;
+
             var usbInterfaceManager = Substitute.For<IUsbInterfaceManager>();
+
+            var usbInterface = Substitute.For<IUsbInterface>();
+            var endpoint = Substitute.For<IUsbInterfaceEndpoint>();
+            usbInterface.UsbInterfaceEndpoints.Add(endpoint);
+            usbInterfaceManager.ReadUsbInterface = usbInterface;
+            usbInterfaceManager.WriteUsbInterface = usbInterface;
+
+            usbInterface.ReadAsync(3).ReturnsForAnyArgs(testreadpacket);
 
             //TODO: Probably shouldn't be relying on this method
             usbInterfaceManager.GetConnectedDeviceDefinitionAsync().ReturnsForAnyArgs(new ConnectedDeviceDefinition(deviceId) { });
 
-            var usbDevice = new UsbDevice(deviceId, usbInterfaceManager, Substitute.For<ILogger>(), Substitute.For<ITracer>());
+            _UsbDevice = new UsbDevice(deviceId, usbInterfaceManager, Substitute.For<ILogger>(), Substitute.For<ITracer>());
 
-            await usbDevice.InitializeAsync();
-
-            Assert.IsNotNull(usbDevice.ConnectedDeviceDefinition);
-            Assert.AreEqual(deviceId, ((ConnectedDeviceDefinition)usbDevice.ConnectedDeviceDefinition).DeviceId);
+            await _UsbDevice.InitializeAsync();
         }
+        #endregion
     }
 }
