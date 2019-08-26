d15dda21909055a0207056bbc49023d90bdf2288
Christian Findlay <a@a.com>
2019-08-26 17:34:01 +1000

    Add stuff

---
+++ src/Hid.Net/Windows/HidService.cs
@@ -0,0 +1,50 @@
+﻿using Device.Net;
+using Microsoft.Win32.SafeHandles;
+using System;
+
+namespace Hid.Net.Windows
+{
+    public class HidService : IHidService
+    {
+        #region Public Properties
+        public ILogger Logger { get; }
+        public SafeFileHandle SafeFileHandle { get; }
+        #endregion
+
+        public HidService(SafeFileHandle safeFileHandle, ILogger logger)
+        {
+            Logger = logger;
+            SafeFileHandle = safeFileHandle;
+        }
+
+        public HidAttributes GetHidAttributes(SafeFileHandle safeFileHandle)
+        {
+            return HidAPICalls.GetHidAttributes(safeFileHandle);
+        }
+
+        public HidCollectionCapabilities GetHidCapabilities(SafeFileHandle readSafeFileHandle)
+        {
+            return HidAPICalls.GetHidCapabilities(readSafeFileHandle);
+        }
+
+        public string GetManufacturer(SafeFileHandle safeFileHandle)
+        {
+            return HidAPICalls.GetManufacturer(safeFileHandle, Logger);
+        }
+
+        public string GetProduct(SafeFileHandle safeFileHandle)
+        {
+            return HidAPICalls.GetProduct(safeFileHandle, Logger);
+        }
+
+        public string GetSerialNumber(SafeFileHandle safeFileHandle)
+        {
+            return HidAPICalls.GetSerialNumber(safeFileHandle, Logger);
+        }
+
+        public Guid GetHidGuid()
+        {
+            return HidAPICalls.GetHidGuid();
+        }
+    }
+}
---
+++ src/Hid.Net/Windows/IHidService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+
+namespace Hid.Net.Windows
+{
+    public interface IHidService
+    {
+        HidAttributes GetHidAttributes(SafeFileHandle safeFileHandle);
+        HidCollectionCapabilities GetHidCapabilities(SafeFileHandle readSafeFileHandle);
+        Guid GetHidGuid();
+        string GetManufacturer(SafeFileHandle safeFileHandle);
+        string GetProduct(SafeFileHandle safeFileHandle);
+        string GetSerialNumber(SafeFileHandle safeFileHandle);
+    }
+}
