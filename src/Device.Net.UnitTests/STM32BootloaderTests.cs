793676b3ee13b1241718cb5390d5dcb8cc752bd6
Christian Findlay <a@a.com>
2019-09-22 17:14:59 +1000

    Create the failing test

---
+++ src/Device.Net.UnitTests/STM32BootloaderTests.cs
@@ -0,0 +1,19 @@
+﻿using LibUsbDotNet;
+using LibUsbDotNet.Main;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Device.Net.IntegrationTests
+{
+    [TestClass]
+    public class STM32BootloaderTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+            //USB\VID_0483&PID_DF11&REV_2200
+            var usbDeviceFinder = new UsbDeviceFinder(0x0483, 0xDF11);
+            var usbDevice = UsbDevice.OpenUsbDevice(usbDeviceFinder);
+            Assert.IsNotNull(usbDevice);
+        }
+    }
+}
