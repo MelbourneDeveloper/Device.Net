94d8bc0f8e9d6cf0b1bdbb3d989926ca7fc6fc72
Christian Findlay <a@a.com>
2020-09-08 16:39:22 +1000

    Consolidate a file that got duplicated

--- src/Usb.Net/UWPUsbDeviceFactory.cs
+++ src/Usb.Net/UWPUsbDeviceFactory.cs
@@ -1,33 +1,28 @@
 ﻿using Device.Net;
-using Device.Net.UWP;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
-namespace Usb.Net.UWP
+namespace Usb.Net
 {
     public delegate Task<IUsbInterfaceManager> GetUsbInterfaceManager(string deviceId);
 
     public static class UsbDeviceFactoryExtensions
     {
         public static IDeviceFactory CreateUsbDeviceFactory(
-            ILoggerFactory loggerFactory,
-            GetConnectedDeviceDefinitionsAsync getConnectedDeviceDefinitionsAsync,
-            GetUsbInterfaceManager getUsbInterfaceManager)
-        {
-            if (loggerFactory == null) throw new ArgumentNullException(nameof(loggerFactory));
-
-            var logger = loggerFactory.CreateLogger<UwpDeviceEnumerator>();
-
-            return new DeviceFactory(
-                loggerFactory,
-                getConnectedDeviceDefinitionsAsync,
-                async (d) =>
-                {
-                    var usbInterfaceManager = await getUsbInterfaceManager(d.DeviceId);
-                    return new UsbDevice(d.DeviceId, usbInterfaceManager, loggerFactory);
-                });
-        }
+        ILoggerFactory loggerFactory,
+        GetConnectedDeviceDefinitionsAsync getConnectedDeviceDefinitionsAsync,
+        GetUsbInterfaceManager getUsbInterfaceManager)
+        => loggerFactory == null
+            ? throw new ArgumentNullException(nameof(loggerFactory))
+            : new DeviceFactory(
+            loggerFactory,
+            getConnectedDeviceDefinitionsAsync,
+            async (d) =>
+            {
+                var usbInterfaceManager = await getUsbInterfaceManager(d.DeviceId);
+                return new UsbDevice(d.DeviceId, usbInterfaceManager, loggerFactory);
+            });
     }
 }
 
