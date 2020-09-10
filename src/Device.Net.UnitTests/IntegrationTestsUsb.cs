c7510165e9ce22bf88acc7e1bece6df16886a0a6
Christian Findlay <a@a.com>
2020-09-10 10:47:05 +1000

    Add the integration test

--- src/Device.Net.UnitTests/IntegrationTestsUsb.cs
+++ src/Device.Net.UnitTests/IntegrationTestsUsb.cs
@@ -17,6 +17,7 @@ namespace Device.Net.UnitTests
         [TestMethod]
         public async Task TestWriteAndReadFromTrezorUsb()
         {
+            //Note: creating a LoggerFactory like this is easier than creating a mock
             var loggerFactory = LoggerFactory.Create((builder) =>
             {
                 _ = builder.AddDebug().SetMinimumLevel(LogLevel.Trace);
@@ -37,9 +38,27 @@ namespace Device.Net.UnitTests
                 },
             });
 
+            //Get the first available device
             var trezorDevice = devices.FirstOrDefault();
 
+            //Ensure that it gets picked up
             Assert.IsNotNull(trezorDevice);
+
+            //Initialize the device
+            await trezorDevice.InitializeAsync();
+
+            //Send the request part of the Message Contract
+            var request = new byte[64];
+            request[0] = 0x3f;
+            request[1] = 0x23;
+            request[2] = 0x23;
+            var response = await trezorDevice.WriteAndReadAsync(request);
+
+            //Specify the response part of the Message Contract
+            var expectedResult = new byte[] { 63, 35, 35, 0, 17, 0, 0, 0, 194, 10, 9, 116, 114, 101, 122, 111, 114, 46, 105, 111, 16, 1, 24, 9, 32, 1, 50, 24, 51, 66, 69, 65, 55, 66, 50, 55, 50, 55, 66, 49, 55, 57, 50, 52, 67, 56, 67, 70, 68, 56, 53, 48, 56, 1, 64, 0, 74, 5, 101, 110, 45, 85, 83, 82 };
+
+            //Assert that the response part meets the specification
+            Assert.IsTrue(expectedResult.SequenceEqual(response.Data));
         }
 
     }
