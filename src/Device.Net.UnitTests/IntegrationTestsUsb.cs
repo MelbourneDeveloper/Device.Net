35e8f3f753eff119a142948def9d34b273716810
Christian Findlay <a@a.com>
2020-09-12 09:20:43 +1000

    Refactor

--- src/Device.Net.UnitTests/IntegrationTestsUsb.cs
+++ src/Device.Net.UnitTests/IntegrationTestsUsb.cs
@@ -12,41 +12,57 @@ namespace Device.Net.UnitTests
     [TestClass]
     public class IntegrationTestsUsb
     {
-        [TestMethod]
-        public async Task TestWriteAndReadFromTrezorUsb()
+        #region Fields
+        private ILoggerFactory _loggerFactory;
+        #endregion
+
+        #region Setup
+        [TestInitialize]
+        public void Setup()
         {
             //Note: creating a LoggerFactory like this is easier than creating a mock
-            var loggerFactory = LoggerFactory.Create((builder) =>
+            _loggerFactory = LoggerFactory.Create((builder) =>
             {
                 _ = builder.AddDebug().SetMinimumLevel(LogLevel.Trace);
             });
+        }
+        #endregion
 
-            //Get the filtered devices
-            var filterDeviceDefinition = new FilterDeviceDefinition
-            {
-                DeviceType = DeviceType.Usb,
-                VendorId = 0x1209,
-                ProductId = 0x53C1,
-                //This does not affect the filtering
-                Label = "Trezor One Firmware 1.7.x"
-            };
-
+        #region Tests
+        [TestMethod]
+        public async Task TestWriteAndReadFromTrezorUsb()
+        {
             //Send the request part of the Message Contract
             var request = new byte[64];
             request[0] = 0x3f;
             request[1] = 0x23;
             request[2] = 0x23;
 
-            var integrationTester = new IntegrationTester(filterDeviceDefinition, new WindowsUsbDeviceFactory(loggerFactory), loggerFactory);
-            await integrationTester.TestAsync(request, async (responseData) =>
-             {
-                 //Specify the response part of the Message Contract
-                 var expectedResult = new byte[] { 63, 35, 35, 0, 17, 0, 0, 0, 194, 10, 9, 116, 114, 101, 122, 111, 114, 46, 105, 111, 16, 1, 24, 9, 32, 1, 50, 24, 51, 66, 69, 65, 55, 66, 50, 55, 50, 55, 66, 49, 55, 57, 50, 52, 67, 56, 67, 70, 68, 56, 53, 48, 56, 1, 64, 0, 74, 5, 101, 110, 45, 85, 83, 82 };
-
-                 //Assert that the response part meets the specification
-                 Assert.IsTrue(expectedResult.SequenceEqual(responseData));
-             });
+            var integrationTester = new IntegrationTester(
+                new FilterDeviceDefinition
+                {
+                    DeviceType = DeviceType.Usb,
+                    VendorId = 0x1209,
+                    ProductId = 0x53C1,
+                    //This does not affect the filtering
+                    Label = "Trezor One Firmware 1.7.x"
+                }, new WindowsUsbDeviceFactory(_loggerFactory), _loggerFactory);
+            await integrationTester.TestAsync(request, AssertResult);
         }
+        #endregion
+
+        #region Private Methods
+        private static Task AssertResult(byte[] responseData)
+        {
+            //Specify the response part of the Message Contract
+            var expectedResult = new byte[] { 63, 35, 35, 0, 17, 0, 0, 0, 194, 10, 9, 116, 114, 101, 122, 111, 114, 46, 105, 111, 16, 1, 24, 9, 32, 1, 50, 24, 51, 66, 69, 65, 55, 66, 50, 55, 50, 55, 66, 49, 55, 57, 50, 52, 67, 56, 67, 70, 68, 56, 53, 48, 56, 1, 64, 0, 74, 5, 101, 110, 45, 85, 83, 82 };
+
+            //Assert that the response part meets the specification
+            Assert.IsTrue(expectedResult.SequenceEqual(responseData));
+
+            return Task.FromResult(true);
+        }
+        #endregion
     }
 }
 
