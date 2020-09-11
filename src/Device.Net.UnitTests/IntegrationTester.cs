9451a9e497bff95e80f9cc8bd006b0fdb54c6884
Christian Findlay <a@a.com>
2020-09-12 08:31:02 +1000

    Start a bit of an integration testing framework

---
+++ src/Device.Net.UnitTests/IntegrationTester.cs
@@ -0,0 +1,25 @@
+﻿#if !NET45
+
+using System;
+using System.Threading.Tasks;
+
+namespace Device.Net.UnitTests
+{
+    public class IntegrationTester
+    {
+        private readonly IDevice _device;
+
+        public IntegrationTester(IDevice device)
+        {
+            _device = device;
+        }
+
+        public async Task TestAsync(byte[] writeData, Func<byte[], Task> assertFunc)
+        {
+            var result = await _device.WriteAndReadAsync(writeData);
+            await assertFunc(result.Data);
+        }
+    }
+}
+
+#endif
--- src/Device.Net.UnitTests/IntegrationTestsUsb.cs
+++ src/Device.Net.UnitTests/IntegrationTestsUsb.cs
@@ -2,6 +2,7 @@
 
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -52,13 +53,18 @@ namespace Device.Net.UnitTests
             request[0] = 0x3f;
             request[1] = 0x23;
             request[2] = 0x23;
-            var response = await trezorDevice.WriteAndReadAsync(request);
 
-            //Specify the response part of the Message Contract
-            var expectedResult = new byte[] { 63, 35, 35, 0, 17, 0, 0, 0, 194, 10, 9, 116, 114, 101, 122, 111, 114, 46, 105, 111, 16, 1, 24, 9, 32, 1, 50, 24, 51, 66, 69, 65, 55, 66, 50, 55, 50, 55, 66, 49, 55, 57, 50, 52, 67, 56, 67, 70, 68, 56, 53, 48, 56, 1, 64, 0, 74, 5, 101, 110, 45, 85, 83, 82 };
+            var integrationTester = new IntegrationTester(trezorDevice);
+            await integrationTester.TestAsync(request, async (responseData) =>
+             {
+                 //Specify the response part of the Message Contract
+                 var expectedResult = new byte[] { 63, 35, 35, 0, 17, 0, 0, 0, 194, 10, 9, 116, 114, 101, 122, 111, 114, 46, 105, 111, 16, 1, 24, 9, 32, 1, 50, 24, 51, 66, 69, 65, 55, 66, 50, 55, 50, 55, 66, 49, 55, 57, 50, 52, 67, 56, 67, 70, 68, 56, 53, 48, 56, 1, 64, 0, 74, 5, 101, 110, 45, 85, 83, 82 };
 
-            //Assert that the response part meets the specification
-            Assert.IsTrue(expectedResult.SequenceEqual(response.Data));
+                 //Assert that the response part meets the specification
+                 Assert.IsTrue(expectedResult.SequenceEqual(responseData));
+             });
+
+
         }
     }
 }
