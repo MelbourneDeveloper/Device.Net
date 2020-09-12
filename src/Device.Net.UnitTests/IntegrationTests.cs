cba7afea5b92430c92ab01b4c0deac0566863e8f
Christian Findlay <a@a.com>
2020-09-12 16:41:14 +1000

    Add the nano test and fix the trezor hid test

--- src/Device.Net.UnitTests/IntegrationTests.cs
+++ src/Device.Net.UnitTests/IntegrationTests.cs
@@ -1,5 +1,6 @@
 #if !NET45
 
+using Hid.Net.Windows;
 using Microsoft.Extensions.Logging;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
@@ -32,15 +33,17 @@ namespace Device.Net.UnitTests
         #region Tests
         [TestMethod]
         public async Task TestWriteAndReadFromTrezorUsb() => TestWriteAndReadFromTrezor(
-                    new FilterDeviceDefinition { DeviceType = DeviceType.Usb, VendorId = 0x1209, ProductId = 0x53C1, Label = "Trezor One Firmware 1.7.x" }
+            new FilterDeviceDefinition { DeviceType = DeviceType.Usb, VendorId = 0x1209, ProductId = 0x53C1, Label = "Trezor One Firmware 1.7.x" },
+            new WindowsUsbDeviceFactory(_loggerFactory)
         );
 
         [TestMethod]
         public async Task TestWriteAndReadFromTrezorHid() => TestWriteAndReadFromTrezor(
-            new FilterDeviceDefinition { DeviceType = DeviceType.Hid, VendorId = 0x534C, ProductId = 0x0001, Label = "Trezor One Firmware 1.6.x", UsagePage = 65280 }
+            new FilterDeviceDefinition { DeviceType = DeviceType.Hid, VendorId = 0x534C, ProductId = 0x0001, Label = "Trezor One Firmware 1.6.x", UsagePage = 65280 },
+            new WindowsHidDeviceFactory(_loggerFactory)
             );
 
-        private async Task TestWriteAndReadFromTrezor(FilterDeviceDefinition filterDeviceDefinition)
+        private async Task TestWriteAndReadFromTrezor(FilterDeviceDefinition filterDeviceDefinition, IDeviceFactory deviceFactory)
         {
             //Send the request part of the Message Contract
             var request = new byte[64];
@@ -49,13 +52,38 @@ namespace Device.Net.UnitTests
             request[2] = 0x23;
 
             var integrationTester = new IntegrationTester(
-                filterDeviceDefinition, new WindowsUsbDeviceFactory(_loggerFactory), _loggerFactory);
-            await integrationTester.TestAsync(request, AssertResult);
+                filterDeviceDefinition, deviceFactory, _loggerFactory);
+            await integrationTester.TestAsync(request, AssertTrezorResult);
+        }
+
+        [TestMethod]
+        public async Task TestWriteAndReadFromNanoHid()
+        {
+            //Send the request part of the Message Contract
+            var request = new byte[64];
+            request[0] = 63;
+            request[1] = 62;
+            request[2] = 1;
+            request[3] = 1;
+            request[4] = 1;
+
+            var integrationTester = new IntegrationTester(
+                new FilterDeviceDefinition
+                {
+                    ProductId = 4112,
+                    VendorId = 10741
+                }, new WindowsHidDeviceFactory(_loggerFactory), _loggerFactory);
+            await integrationTester.TestAsync(request, async (a) =>
+             {
+                 Assert.AreEqual(64, a.Length);
+                 Assert.AreEqual(63, a[0]);
+                 Assert.AreEqual(62, a[1]);
+             });
         }
         #endregion
 
         #region Private Methods
-        private static Task AssertResult(byte[] responseData)
+        private static Task AssertTrezorResult(byte[] responseData)
         {
             //Specify the response part of the Message Contract
             var expectedResult = new byte[] { 63, 35, 35, 0, 17, 0, 0, 0, 194, 10, 9, 116, 114, 101, 122, 111, 114, 46, 105, 111, 16, 1, 24, 9, 32, 1, 50, 24, 51, 66, 69, 65, 55, 66, 50, 55, 50, 55, 66, 49, 55, 57, 50, 52, 67, 56, 67, 70, 68, 56, 53, 48, 56, 1, 64, 0, 74, 5, 101, 110, 45, 85, 83, 82 };
