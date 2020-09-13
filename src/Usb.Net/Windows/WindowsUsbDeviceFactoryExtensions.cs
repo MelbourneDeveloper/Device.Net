b89273a1d31b5da5d28f13457c46a0c7d6220631
Christian Findlay <a@a.com>
2020-09-13 11:23:59 +1000

    Windows USB device factory mostly functional

---
+++ src/Usb.Net/Windows/WindowsUsbDeviceFactoryExtensions.cs
@@ -0,0 +1,82 @@
+﻿using Device.Net;
+using Device.Net.Windows;
+using Microsoft.Extensions.Logging;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace Usb.Net.Windows
+{
+    public static class WindowsUsbDeviceFactoryExtensions
+    {
+        public static IDeviceFactory CreateWindowsUsbDeviceFactory(
+        this IEnumerable<FilterDeviceDefinition> filterDeviceDefinitions,
+        ILoggerFactory loggerFactory,
+        GetConnectedDeviceDefinitionsAsync getConnectedDeviceDefinitionsAsync = null,
+        GetUsbInterfaceManager getUsbInterfaceManager = null,
+        Guid? classGuid = null,
+        ushort? readBufferSize = null,
+        ushort? writeBufferSize = null
+    )
+        {
+            if (getConnectedDeviceDefinitionsAsync == null)
+            {
+                var uwpHidDeviceEnumerator = new WindowsDeviceEnumerator(
+                    loggerFactory.CreateLogger<WindowsDeviceEnumerator>(),
+                    classGuid ?? WindowsDeviceConstants.WinUSBGuid,
+                    GetDeviceDefinition,
+                    async (c) =>
+                    filterDeviceDefinitions.FirstOrDefault((f) => DeviceManager.IsDefinitionMatch(f, c, DeviceType.Usb)) != null);
+
+                getConnectedDeviceDefinitionsAsync = uwpHidDeviceEnumerator.GetConnectedDeviceDefinitionsAsync;
+            }
+
+            if (getUsbInterfaceManager == null)
+            {
+                getUsbInterfaceManager = async (d) =>
+                    new WindowsUsbInterfaceManager(
+                    //TODO: no idea if this is OK...
+                    d,
+                    loggerFactory,
+                    readBufferSize,
+                    writeBufferSize);
+            }
+
+            return UsbDeviceFactoryExtensions.CreateUsbDeviceFactory(loggerFactory, getConnectedDeviceDefinitionsAsync, getUsbInterfaceManager);
+        }
+
+        internal static ConnectedDeviceDefinition GetDeviceDefinition(SafeFileHandle defaultInterfaceHandle, string deviceId)
+        {
+            var deviceDefinition = new ConnectedDeviceDefinition(deviceId) { DeviceType = DeviceType.Usb };
+
+            var bufferLength = (uint)Marshal.SizeOf(typeof(USB_DEVICE_DESCRIPTOR));
+            var isSuccess2 = WinUsbApiCalls.WinUsb_GetDescriptor(defaultInterfaceHandle, WinUsbApiCalls.DEFAULT_DESCRIPTOR_TYPE, 0, WinUsbApiCalls.EnglishLanguageID, out var _UsbDeviceDescriptor, bufferLength, out var lengthTransferred);
+            WindowsDeviceBase.HandleError(isSuccess2, "Couldn't get device descriptor");
+
+            if (_UsbDeviceDescriptor.iProduct > 0)
+            {
+                deviceDefinition.ProductName = WinUsbApiCalls.GetDescriptor(defaultInterfaceHandle, _UsbDeviceDescriptor.iProduct, "Couldn't get product name");
+            }
+
+            if (_UsbDeviceDescriptor.iSerialNumber > 0)
+            {
+                deviceDefinition.SerialNumber = WinUsbApiCalls.GetDescriptor(defaultInterfaceHandle, _UsbDeviceDescriptor.iSerialNumber, "Couldn't get serial number");
+            }
+
+            if (_UsbDeviceDescriptor.iManufacturer > 0)
+            {
+                deviceDefinition.Manufacturer = WinUsbApiCalls.GetDescriptor(defaultInterfaceHandle, _UsbDeviceDescriptor.iManufacturer, "Couldn't get manufacturer");
+            }
+
+            deviceDefinition.VendorId = _UsbDeviceDescriptor.idVendor;
+            deviceDefinition.ProductId = _UsbDeviceDescriptor.idProduct;
+            deviceDefinition.WriteBufferSize = _UsbDeviceDescriptor.bMaxPacketSize0;
+            deviceDefinition.ReadBufferSize = _UsbDeviceDescriptor.bMaxPacketSize0;
+
+            return deviceDefinition;
+        }
+
+    }
+}
