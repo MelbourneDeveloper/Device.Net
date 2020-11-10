63542d0d409e54e23864b962813d08aaa689a190
Christian Findlay <a@a.com>
2020-11-10 18:19:34 +1100

    Add the setup packet

---
+++ src/Usb.Net/SetupPacket.cs
@@ -0,0 +1,32 @@
+﻿namespace Usb.Net
+{
+    public class SetupPacket
+    {
+        #region Public Properties
+        public byte RequestType { get; }
+        public byte Request { get; }
+        public ushort Value { get; }
+        public ushort Index { get; }
+        public ushort Length { get; }
+        #endregion
+
+        #region Constructors
+        public SetupPacket
+            (
+         byte requestType,
+         byte request,
+         ushort value,
+         ushort index,
+         ushort length
+            )
+        {
+            RequestType = requestType;
+            Request = request;
+            Value = value;
+            Index = index;
+            Length = length;
+        }
+        #endregion
+
+    }
+}
