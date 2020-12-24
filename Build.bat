3e56ea1f7a0f87a4fb0dc3c4d7fed0ec84de79c9
Christian Findlay <email@email.email>
2020-12-24 18:47:02 +1100

    Correct the batch file

--- Build.bat
+++ Build.bat
@@ -1,12 +1,12 @@
 git clean -x -f -d
 
-"c:\temp\nuget restore" src/Device.Net.Pipelines.sln
+"c:\temp\nuget" restore src/Device.Net.Pipelines.sln
 
 msbuild src/Device.Net.Pipelines.sln /property:Configuration=Release
 
-"c:\temp\nuget pack" Build/NuSpecs/Device.Net.nuspec -Symbols -SymbolPackageFormat snupkg
-"c:\temp\nuget pack" Build/NuSpecs/Device.Net.LibUsb.nuspec -Symbols -SymbolPackageFormat snupkg
-"c:\temp\nuget pack" Build/NuSpecs/Hid.Net.nuspec -Symbols -SymbolPackageFormat snupkg
-"c:\temp\nuget pack" Build/NuSpecs/SerialPort.Net.nuspec -Symbols -SymbolPackageFormat snupkg
-"c:\temp\nuget pack" Build/NuSpecs/Usb.Net.nuspec -Symbols -SymbolPackageFormat snupkg
+"c:\temp\nuget" pack Build/NuSpecs/Device.Net.nuspec -Symbols -SymbolPackageFormat snupkg
+"c:\temp\nuget" pack Build/NuSpecs/Device.Net.LibUsb.nuspec -Symbols -SymbolPackageFormat snupkg
+"c:\temp\nuget" pack Build/NuSpecs/Hid.Net.nuspec -Symbols -SymbolPackageFormat snupkg
+"c:\temp\nuget" pack Build/NuSpecs/SerialPort.Net.nuspec -Symbols -SymbolPackageFormat snupkg
+"c:\temp\nuget" pack Build/NuSpecs/Usb.Net.nuspec -Symbols -SymbolPackageFormat snupkg
 
