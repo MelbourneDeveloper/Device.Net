1edf7e5802dcd81eb4915abe698c82ca0ab86680
Christian Findlay <email@email.email>
2021-02-02 07:54:42 +1100

    A nice stub

---
+++ src/Device.Net.WebUSB/WebUSBFactoryExtensions.cs
@@ -0,0 +1,51 @@
+﻿using Blazor.Extensions.WebUSB;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Usb.Net;
+
+namespace Device.Net.WebUSB
+{
+    public static class WebUSBFactoryExtensions
+    {
+        public static IDeviceFactory CreateWebUSBDeviceFactory(
+        this IEnumerable<FilterDeviceDefinition> filterDeviceDefinitions,
+        IUSB usb,
+        ILoggerFactory loggerFactory = null,
+        GetConnectedDeviceDefinitionsAsync getConnectedDeviceDefinitionsAsync = null,
+        GetUsbInterfaceManager getUsbInterfaceManager = null
+        )
+        {
+            if (filterDeviceDefinitions == null) throw new ArgumentNullException(nameof(filterDeviceDefinitions));
+
+            loggerFactory ??= NullLoggerFactory.Instance;
+
+            getConnectedDeviceDefinitionsAsync ??= (c) => GetConnectedDevicesAsync(usb);
+
+            //getUsbInterfaceManager ??=
+
+            return UsbDeviceFactoryExtensions.CreateUsbDeviceFactory(getConnectedDeviceDefinitionsAsync, getUsbInterfaceManager, loggerFactory);
+        }
+
+        private static async Task<IEnumerable<ConnectedDeviceDefinition>> GetConnectedDevicesAsync(IUSB usb)
+        {
+            var devices = await usb.GetDevices().ConfigureAwait(false);
+
+            IEnumerable<ConnectedDeviceDefinition> connectedDeviceDefinitions = null;
+
+            connectedDeviceDefinitions = devices.Select(d => new ConnectedDeviceDefinition(
+                d.SerialNumber,
+                DeviceType.Usb,
+                (uint)d.VendorId,
+                (uint)d.ProductId,
+                d.ProductName,
+                d.ManufacturerName,
+                d.SerialNumber));
+
+            return connectedDeviceDefinitions;
+        }
+    }
+}
