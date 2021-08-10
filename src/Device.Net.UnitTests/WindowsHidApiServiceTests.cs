da9be364382e54527877c04d76dbfc03d391e1d0
Brendon Parker <bparker@icsfl.com>
2021-08-10 13:48:21 -0400

    Add failing unit test to illustrate bug when write buffer is zero.

---
+++ src/Device.Net.UnitTests/WindowsHidApiServiceTests.cs
@@ -0,0 +1,33 @@
+﻿using Hid.Net.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32.SafeHandles;
+using System.IO;
+
+namespace Device.Net.UnitTests
+{
+    [TestClass]
+    public class WindowsHidApiServiceTests
+    {
+        private SafeFileHandle CreateFileHandle()
+        {
+            var file = File.Create(Path.GetTempFileName(), 100, FileOptions.Asynchronous);
+            return file.SafeFileHandle;
+        }
+
+        [TestMethod]
+        public void OpenWriteDoesNotThrowWhenZeroLengthBuffer()
+        {
+            var windowsHidApiService = new WindowsHidApiService(null);
+            using var handle = CreateFileHandle();
+            _ = windowsHidApiService.OpenWrite(handle, 0);
+        }
+
+        [TestMethod]
+        public void OpenWriteDoesNotThrowWhenMoreThanZero()
+        {
+            var windowsHidApiService = new WindowsHidApiService(null);
+            using var handle = CreateFileHandle();
+            _ = windowsHidApiService.OpenWrite(handle, 10);
+        }
+    }
+}
