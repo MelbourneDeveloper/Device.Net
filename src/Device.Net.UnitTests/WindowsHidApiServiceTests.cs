b255549b2afebd467c02126b598dcbe61d991b02
Brendon Parker <bparker@icsfl.com>
2021-08-10 15:41:16 -0400

    Add failing test for scenario when write buffer length is zero.

---
+++ src/Device.Net.UnitTests/WindowsHidApiServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using Device.Net.Windows;
+using Hid.Net.Windows;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32.SafeHandles;
+using System;
+using System.IO;
+
+namespace Device.Net.UnitTests
+{
+    [TestClass]
+    public class WindowsHidApiServiceTests
+    {
+        private SafeFileHandle CreateFileHandle()
+        {
+            return APICalls.CreateFile(Path.GetTempFileName(),
+                    FileAccessRights.GenericWrite | FileAccessRights.GenericRead,
+                    APICalls.FileShareRead | APICalls.FileShareWrite, IntPtr.Zero, APICalls.OpenExisting,
+                    APICalls.FileAttributeNormal | APICalls.FileFlagOverlapped, IntPtr.Zero);
+        }
+
+        [TestMethod]
+        public void OpenWriteDoesNotThrowWhenZeroLengthBuffer()
+        {
+            var windowsHidApiService = new WindowsHidApiService(null);
+            using var handle = CreateFileHandle();
+            _ = windowsHidApiService.OpenWrite(handle, 0);
+        }
+
+        [TestMethod]
+        public void OpenWriteDoesNotThrowWhenMoreThanZero()
+        {
+            var windowsHidApiService = new WindowsHidApiService(null);
+            using var handle = CreateFileHandle();
+            _ = windowsHidApiService.OpenWrite(handle, 10);
+        }
+    }
+}
